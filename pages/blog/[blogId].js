4e3461198a3e3eada6721c7c18f9245a19cb1fb9
Charan R <SC0024@fbspl.com>
2020-03-03 09:51:58 +0530

    changes

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -1,40 +1,77 @@
-import { Component } from "react";
 import Link from "next/link";
 import Head from "next/head";
 import fetch from "isomorphic-unfetch";
 
-export default class extends Component {
-  static async getInitialProps(query) {
-    // fetch single post detail
-    console.log("qyeryss", query.asPath.split("/")[2]);
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/posts/${query.asPath.split("/")[2]}`
-    );
-    const post = await response.json();
-    return { ...post };
-  }
+ const Blog = (props) => {
+  const { title, body = "default body gettins" } = props;
 
-  render() {
-    const { title, body = "default body gettins" } = this.props;
+  return (
+    <main>
+    <Head>
+      <title>{title}</title>
+      <meta
+        name="description"
+        content={body}
+      />
+    </Head>
 
-    return (
-      <main>
-        <Head>
-          <title>{title}</title>
-          <meta
-            name="description"
-            content={body}
-          />
-        </Head>
+    <h1>{title}</h1>
 
-        <h1>{title}</h1>
+    <p>{body}</p>
 
-        <p>{body}</p>
+    <Link href="/" as={process.env.BACKEND_URL + "/"}>
+      <a>Go back to home</a>
+    </Link>
+  </main>
 
-        <Link href="/" as={process.env.BACKEND_URL + "/"}>
-          <a>Go back to home</a>
-        </Link>
-      </main>
-    );
-  }
+  )
 }
+Blog.getInitialProps = async ( query ) => {
+  console.log("qyeryss", query);
+
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${query.asPath.split("/")[2]}`
+  );
+  const post = await response.json();
+  return { ...post };
+}
+
+export default Blog
+
+
+
+// export default class extends Component {
+//   static async getInitialProps(query) {
+//     // fetch single post detail
+//     console.log("qyeryss", query.asPath.split("/")[2]);
+//     const response = await fetch(
+//       `https://jsonplaceholder.typicode.com/posts/${query.asPath.split("/")[2]}`
+//     );
+//     const post = await response.json();
+//     return { ...post };
+//   }
+
+//   render() {
+//     const { title, body = "default body gettins" } = this.props;
+
+//     return (
+//       <main>
+//         <Head>
+//           <title>{title}</title>
+//           <meta
+//             name="description"
+//             content={body}
+//           />
+//         </Head>
+
+//         <h1>{title}</h1>
+
+//         <p>{body}</p>
+
+//         <Link href="/" as={process.env.BACKEND_URL + "/"}>
+//           <a>Go back to home</a>
+//         </Link>
+//       </main>
+//     );
+//   }
+// }
